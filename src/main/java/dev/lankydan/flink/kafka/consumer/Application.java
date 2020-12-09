f2854556a3541a5a8e2936f0a772001d690ab53c
LankyDan <danknewton@hotmail.com>
2020-12-09 15:43:05 +0000

    Output result of tweet filtering

--- src/main/java/dev/lankydan/flink/kafka/consumer/Application.java
+++ src/main/java/dev/lankydan/flink/kafka/consumer/Application.java
@@ -18,6 +18,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -63,12 +64,17 @@ public class Application {
             AsyncDataStream.unorderedWait(tweetDataStream, new EnrichTweet(), 5000, TimeUnit.MILLISECONDS)
                 .map(new ConvertJsonIntoEnrichedTweet())
                 // Ignore rate limit errors
-                .filter(Objects::nonNull);
+                .filter(Objects::nonNull)
+                // some enriched tweets don't have data, not sure why but filter them out anyway
+                .filter(tweet -> tweet.getData() != null)
+                // some enriched tweets don't have authors, not sure why but filter them out anyway
+                .filter(tweet -> tweet.getData().get(0).getAuthorId() != null);
 //                .keyBy(tweet -> tweet.getData().get(0).getAuthorId());
 
-        DataStream<Tuple2<EnrichedTweet, EnrichedTweet>> withAuthorTweets =
+        DataStream<Result> withAuthorTweets =
             AsyncDataStream.unorderedWait(enriched, new GetRecentAuthorTweets(), 5000, TimeUnit.MILLISECONDS)
-            .filter(new FilterByRepeatedMentions());
+                .filter(new FilterByRepeatedMentions())
+                .map(new MapToResult());
 
         withAuthorTweets.print();
         environment.execute();
@@ -143,16 +149,25 @@ public class Application {
 
         private transient ObjectMapper mapper;
         private transient TwitterClient client;
+        private transient Logger log;
 
         @Override
         public void open(Configuration parameters) throws Exception {
             mapper = new ObjectMapper();
             mapper.registerModule(new JavaTimeModule());
             client = TwitterClient.create();
+            log = LoggerFactory.getLogger(GetRecentAuthorTweets.class);
         }
 
         @Override
         public void asyncInvoke(EnrichedTweet input, ResultFuture<Tuple2<EnrichedTweet, EnrichedTweet>> resultFuture) {
+            if (input.getData() == null) {
+                log.info("Data was null: {}", input);
+            } else if (input.getData().get(0) == null) {
+                log.info("Data .get(0) was null: {}", input.getData());
+            } else if (input.getData().get(0).getAuthorId() == null) {
+                log.info("Author was null: {}", input.getData().get(0));
+            }
             client.getRecentTweetsByAuthor(input.getData().get(0).getAuthorId())
                 .thenAccept(result -> {
                     try {
@@ -176,34 +191,100 @@ public class Application {
 
         @Override
         public boolean filter(Tuple2<EnrichedTweet, EnrichedTweet> value) throws Exception {
-            EnrichedTweet streamedTweet = value.f0;
+            EnrichedTweetData streamedTweetData = value.f0.getData().get(0);
             EnrichedTweet authorTweets = value.f1;
 
-            if (streamedTweet.getData() == null) {
-                log.warn("Streamed tweet was null, {}", streamedTweet);
-                return false;
+//            if (streamedTweetData == null) {
+//                log.warn("Streamed tweet was null, {}", value.f0);
+//                return false;
+//            } else if (streamedTweet.getData().get(0) == null) {
+//                log.warn("Streamed tweet data .get(0) was null, {}", streamedTweet.getData());
+//                return false;
+//            }
+
+            // entities null
+            try {
+                Entities entities = streamedTweetData.getEntities();
+                Set<String> streamedTweetMentions;
+                if (entities != null && entities.getMentions() != null) {
+
+                    streamedTweetMentions = entities
+                        .getMentions()
+                        .stream()
+                        .map(Mention::getUsername)
+                        .collect(Collectors.toSet());
+                } else {
+                    streamedTweetMentions = Collections.emptySet();
+                }
+
+                if (authorTweets.getData() == null) {
+                    log.warn("Author tweets was null, {}", authorTweets.getData());
+                    return false;
+                }
+
+                Set<String> authorTweetMentions = authorTweets.getData().stream()
+                    .filter(data -> !data.getId().equals(streamedTweetData.getId()))
+                    .map(data -> {
+                        if (data.getEntities() != null && data.getEntities().getMentions() != null) {
+                            return data.getEntities().getMentions();
+                        } else {
+                            return Collections.<Mention>emptyList();
+                        }
+                    })
+                    .flatMap(Collection::stream)
+                    .map(Mention::getUsername)
+                    .collect(Collectors.toSet());
+
+                return CollectionUtils.containsAny(streamedTweetMentions, authorTweetMentions);
+            } catch (NullPointerException e) {
+                throw e;
             }
+        }
+    }
 
-            Set<String> streamedTweetMentions = streamedTweet.getData().get(0)
-                .getEntities()
-                .getMentions()
-                .stream()
-                .map(Mention::getUsername)
-                .collect(Collectors.toSet());
+    public static class MapToResult extends RichMapFunction<Tuple2<EnrichedTweet, EnrichedTweet>, Result> {
 
-            if (authorTweets.getData() == null) {
-                log.warn("Author tweets was null, {}", streamedTweet);
-                return false;
+        @Override
+        public Result map(Tuple2<EnrichedTweet, EnrichedTweet> value) throws Exception {
+            EnrichedTweetData streamedTweetData = value.f0.getData().get(0);
+            EnrichedTweet authorTweets = value.f1;
+
+            List<String> streamedTweetMentions;
+            if (streamedTweetData.getEntities() != null && streamedTweetData.getEntities().getMentions() != null) {
+                streamedTweetMentions =
+                    streamedTweetData.getEntities().getMentions().stream().map(Mention::getUsername).collect(Collectors.toList());
+            } else {
+                streamedTweetMentions = Collections.emptyList();
             }
+            Result.Tweet tweet = new Result.Tweet(
+                streamedTweetMentions,
+                streamedTweetData.getPublicMetrics().getRetweetCount(),
+                streamedTweetData.getPublicMetrics().getReplyCount(),
+                streamedTweetData.getPublicMetrics().getLikeCount(),
+                streamedTweetData.getPublicMetrics().getQuoteCount(),
+                streamedTweetData.getText()
+            );
 
-            Set<String> authorTweetMentions = authorTweets.getData().stream()
-                .filter(data -> !data.getId().equals(streamedTweet.getData().get(0).getId()))
-                .map(data -> data.getEntities().getMentions())
-                .flatMap(Collection::stream)
-                .map(Mention::getUsername)
-                .collect(Collectors.toSet());
+            List<Result.Tweet> tweets = authorTweets.getData().stream().map(data -> {
+                List<String> mentions;
+                if (data.getEntities() != null && data.getEntities().getMentions() != null) {
+                    mentions = data.getEntities().getMentions().stream().map(Mention::getUsername).collect(Collectors.toList());
+                } else {
+                    mentions = Collections.emptyList();
+                }
+                return new Result.Tweet(
+                    mentions,
+                    data.getPublicMetrics().getRetweetCount(),
+                    data.getPublicMetrics().getReplyCount(),
+                    data.getPublicMetrics().getLikeCount(),
+                    data.getPublicMetrics().getQuoteCount(),
+                    data.getText()
+                );
+            }).collect(Collectors.toList());
 
-            return CollectionUtils.containsAny(streamedTweetMentions, authorTweetMentions);
+            tweets.add(tweet);
+
+            return new Result(streamedTweetData.getAuthorId(), tweets);
         }
     }
 
---
+++ src/main/java/dev/lankydan/flink/kafka/consumer/Result.java
@@ -0,0 +1,54 @@
+package dev.lankydan.flink.kafka.consumer;
+
+import java.util.List;
+
+public class Result {
+
+    private String authorId;
+    private List<Tweet> tweets;
+
+    public Result(String authorId, List<Tweet> tweets) {
+        this.authorId = authorId;
+        this.tweets = tweets;
+    }
+
+    @Override
+    public String toString() {
+        return "========\n" +
+            "Result{" +
+            "authorId='" + authorId + '\'' +
+            ",\n  tweets=" + tweets +
+            "\n}\n" +
+            "========\n";
+    }
+
+    static class Tweet {
+        private List<String> mentions;
+        private int retweetCount;
+        private int replyCount;
+        private int likeCount;
+        private int quoteCount;
+        private String text;
+
+        public Tweet(List<String> mentions, int retweetCount, int replyCount, int likeCount, int quoteCount, String text) {
+            this.mentions = mentions;
+            this.retweetCount = retweetCount;
+            this.replyCount = replyCount;
+            this.likeCount = likeCount;
+            this.quoteCount = quoteCount;
+            this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return "Tweet{" +
+                "mentions=" + mentions +
+                ", retweetCount=" + retweetCount +
+                ", replyCount=" + replyCount +
+                ", likeCount=" + likeCount +
+                ", quoteCount=" + quoteCount +
+                ", text='" + text + '\'' +
+                "}\n";
+        }
+    }
+}
