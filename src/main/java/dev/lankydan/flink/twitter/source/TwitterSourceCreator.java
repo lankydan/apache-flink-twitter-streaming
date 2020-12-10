5644fb298a0b858566cafd84a4495d62f81d20f8
LankyDan <danknewton@hotmail.com>
2020-12-10 23:26:15 +0000

    Small corrections to `TwitterSourceCreator`

--- src/main/java/dev/lankydan/flink/twitter/source/TwitterSourceCreator.java
+++ src/main/java/dev/lankydan/flink/twitter/source/TwitterSourceCreator.java
@@ -1,6 +1,5 @@
 package dev.lankydan.flink.twitter.source;
 
-import dev.lankydan.flink.twitter.client.TwitterClient;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 
 import java.io.IOException;
@@ -11,14 +10,14 @@ public class TwitterSourceCreator {
     public static TwitterSource create() {
         try {
             Properties properties = new Properties();
-            properties.load(TwitterClient.class.getClassLoader().getResourceAsStream("application.properties"));
+            properties.load(TwitterSource.class.getClassLoader().getResourceAsStream("application.properties"));
             properties.setProperty(TwitterSource.CONSUMER_KEY, properties.getProperty("api.key"));
             properties.setProperty(TwitterSource.CONSUMER_SECRET, properties.getProperty("api.secret.key"));
             properties.setProperty(TwitterSource.TOKEN, properties.getProperty("api.token"));
             properties.setProperty(TwitterSource.TOKEN_SECRET, properties.getProperty("api.secret.token"));
             return new TwitterSource(properties);
         } catch (IOException e) {
-            throw new RuntimeException("Couldn't load prpoerties");
+            throw new RuntimeException("Couldn't load properties");
         }
     }
 }
